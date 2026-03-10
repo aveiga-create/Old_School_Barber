0755f3cedd079b2f7125ef7234e06fefdc34ae51
Anderson Veiga <andersonamaral@aluno.santoangelo.uri.br>
2026-03-10 19:34:13 -0300

    Atualizações

--- models.py
+++ models.py
@@ -41,16 +41,15 @@ class Barbeiro(db.Model):
     foto = db.Column(db.String(200))
     ativo = db.Column(db.Boolean, default=True)
 
-   agendamentos = db.relationship(
-    "Agendamento",
-    back_populates="barbeiro",
-    lazy="select",
-    cascade="all, delete-orphan"
-)
-
+    agendamentos = db.relationship(
+        "Agendamento",
+        back_populates="barbeiro",
+        cascade="all, delete-orphan"
+    )
 
     def __repr__(self):
         return f"<Barbeiro {self.nome}>"
+
 
 
 # ==========================================
@@ -83,21 +82,19 @@ class Agendamento(db.Model):
     data = db.Column(db.Date, nullable=False)
     horario = db.Column(db.Time, nullable=False)
 
-    # STATUS DO AGENDAMENTO
     status = db.Column(db.String(50), default="Confirmado")
-
-    # PAGAMENTO
     forma_pagamento = db.Column(db.String(20), nullable=False)
     status_pagamento = db.Column(db.String(20), default="Pendente")
 
     criado_em = db.Column(db.DateTime, default=datetime.utcnow)
 
-    # Relacionamentos
+    barbeiro = db.relationship(
+        "Barbeiro",
+        back_populates="agendamentos"
+    )
+
     servico = db.relationship("Servico", lazy="joined")
-    barbeiro = db.relationship("Barbeiro", back_populates="agendamentos")
 
-
-    # Impede dois agendamentos no mesmo horário para o mesmo barbeiro
     __table_args__ = (
         db.UniqueConstraint("barbeiro_id", "data", "horario", name="unique_agendamento"),
     )
